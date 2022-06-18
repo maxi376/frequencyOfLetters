d0103efec58df6b049c3991323e75931bc916b16
maxi333 <max.timofte@example.com>
2022-06-18 08:48:13 +0300

    comm3

--- src/main/java/FrequencyOfLetters.java
+++ src/main/java/FrequencyOfLetters.java
@@ -1,34 +1,36 @@
 import java.util.Arrays;
+
 public class FrequencyOfLetters {
-    public static void main(String[] args){
-        String s="abrxkbassdfirsdqapqoiuwrapjaoc";
+    public static void main(String[] args) {
+        String s = "abrxkbassdfirsdqapqoiuwrapjaoc";
         int n = 30;
-        int[] F=new int[52];
-        Arrays.fill(F,0);
-        
-        F= FrequencyOfLetters.frequencyOfLetters(s,n);
-        for(int i=0;i<26;i++)
-            System.out.print((char)(i+65)+" ");
+        int[] F = new int[52];
+        Arrays.fill(F, 0);
+
+        F = FrequencyOfLetters.frequencyOfLetters(s, n);
+        for (int i = 0; i < 26; i++)
+            System.out.print((char) (i + 65) + " ");
         System.out.println();
-        for(int i=0;i<26;i++)
-            System.out.print(F[i]+" ");
+        for (int i = 0; i < 26; i++)
+            System.out.print(F[i] + " ");
         System.out.println();
-        for(int i=0;i<26;i++)
-            System.out.print((char)(i+97)+" ");
+        for (int i = 0; i < 26; i++)
+            System.out.print((char) (i + 97) + " ");
         System.out.println();
-        for(int i=26;i<52;i++)
-            System.out.print(F[i]+" ");
+        for (int i = 26; i < 52; i++)
+            System.out.print(F[i] + " ");
     }
-    public static int[]  frequencyOfLetters(String s, int n){
-        int[] F=new int[52];
+
+    public static int[] frequencyOfLetters(String s, int n) {
+        int[] F = new int[52];
         char a;
-        for(int i=0; i<s.length(); i++){
-            a=s.charAt(i);
+        for (int i = 0; i < s.length(); i++) {
+            a = s.charAt(i);
             if (Character.isLetter(a)) {
-                if (Character.isUpperCase(a)){
-                    F[(int)a - 65]++;
-                }else if (Character.isLowerCase(a)){//26 pt upper-> 97-26=71
-                    F[(int)a - 71]++;
+                if (Character.isUpperCase(a)) {
+                    F[(int) a - 65]++;
+                } else if (Character.isLowerCase(a)) {//26 pt upper-> 97-26=71
+                    F[(int) a - 71]++;
                 }
             }
         }
